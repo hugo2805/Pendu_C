933feff11258f051335ff91c6971a0c85a87b2a5
hugo2805 <56994115+hugo2805@users.noreply.github.com>
2020-11-20 14:22:07 +0100

    Update 1
    Mise en place des fonctions et procedures

--- main.c
+++ main.c
@@ -1,57 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TAILLE 7
 #define ERREUR 8
 
-int main()
-{
+char demande_char();
+void affiche_instruction();
+int verif(char);
+int MaJ(int , int , int , char);
+
     char tab_base1[TAILLE] = {'I','P','H','O','N','E'};
     char tab_base2[TAILLE] = {'i','p','h','o','n','e'};
     char tab_affiche[TAILLE]= {'-','-','-','-','-','-'};
     char tab_faux[ERREUR]= {'-','-','-','-','-','-','-'};
-    char car_prop;
-    int i,j;
+
+
+int main()
+{
+
+    char car_prop = ' ';
+    int isJuste;
+    int j = 0;
     int essai=1;
-    int faux;
+    int faux= 7;
 
     printf("Bienvenue dans cette version du pendu en C\n");
     printf("Le mot que vous devez trouver contient %d lettres\n",TAILLE-1);
 
-    while (tab_affiche != tab_base1 || faux < 7) {
+    while (tab_affiche != tab_base1)
+        {
+        fflush(stdin);
+        affiche_instruction(essai);
+        car_prop = demande_char();
+        isJuste = verif(car_prop);
+        faux = MaJ( isJuste, faux, j, car_prop);
 
+        j++;
+        essai++;
+
+        }
+
+
+
+
+
+
+    return 0;
+}
+
+void affiche_instruction(int essai)
+{
     printf("|| ESSAI No : %d ||\n", essai);
     printf("Vous avez des essaye : %s\n", tab_faux);
     printf("==========================================================\n");
     printf("%s\n",tab_affiche);
     printf("==========================================================\n\n");
     printf("Pour proposer un caractere, entrez le dans le champ suivant : \n");
-    scanf("%c",&car_prop);
-    for (i=0;i<7;i++)
+}
+
+char demande_char()
+{
+    char car_prop;
+   scanf("%c",&car_prop);
+
+   return car_prop;
+}
+
+int verif(char car_prop)
+{
+    int i;
+
+       for (i=0;i<strlen(tab_base1);i++)
     {
-        if (car_prop == tab_base1[i] || car_prop == tab_base2[i] )
+        if ((car_prop == tab_base1[i]) || (car_prop == tab_base2[i]) )
             {
                 tab_affiche[i]=tab_base1[i];
-                printf("Bravo vous avez trouve une Lettre\n");
+
+               fflush(stdin);
+
+               return 1;
 
             }
-            else
+
+           if ((car_prop != tab_base1[i]) && (car_prop != tab_base2[i]) )
             {
-                printf("Dommage essayez encore !\n");
-                faux += 1;
-                tab_faux[j]= car_prop;
+               return 0;
+
             }
 
     }
+}
 
-    essai++;
-    j++;
+int MaJ(int isJuste, int faux, int j, char car_prop)
+{
+    if (!isJuste)
+        {
+        faux --;
+    tab_faux[j]= car_prop;
+    printf("OUPS! Tentez encore votre chance !\n");
 
-    }
 
+        }
 
+        if (isJuste)
+        {
+            printf("Bravo !!!! Vous avez trouve une lettre!\n");
+        }
 
+        return faux;
 
-    return 0;
 }
 
 
